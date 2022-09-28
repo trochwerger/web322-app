70e9cea5871b181172f938f1f9ab5cecce8df360
Tomas <tomasrochwerger3@gmail.com>
2022-09-28 15:20:22 -0400

    Part-1 Commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+var app = express();
+
+var path = require("path");
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+// call this function after the http server starts listening for requests
+function onHttpStart() {
+  console.log("Express http server listening on port: " + HTTP_PORT);
+}
+app.use(express.static('public')); 
+// setup a 'route' to listen on the default url path (http://localhost)
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/home.html"));
+});
+
+// setup another route to listen on /about
+app.get("/about", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/about.html"));
+});
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT, onHttpStart);
