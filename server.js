e8436d0321080be796ea1a7f6634d03f47ababc2
Tomas <tomasrochwerger3@gmail.com>
2022-09-30 22:46:23 -0400

    Final Commit

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Tomas Rochwerger Student ID: 159432210 Date: 09/30/2022
+*
+*  Online (Cyclic) Link: 
+*
+********************************************************************************/ 
+
 const express = require("express");
 const data = require("./data-service.js");
 var app = express();
